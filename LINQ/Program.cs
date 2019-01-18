60094ded30d42dccffe12333658f0d773c360952
shalina2 <shalom.belaineh@gmail.com>
2019-01-18 10:00:36 -0800

    working

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,13 +1,31 @@
-﻿using System;
+﻿using LINQ.Classes;
+using System;
+using System.Collections.Generic;
 
 namespace LINQ
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             string Path = ("./../../../../../../../Linq.JSON ");
-         }
+
+            ///1.Output all of the neighborhoods in this data list
+
+            List<Properties> list = GetNeighbors(Getobj());
+            Print(list);
+            ///2.Filter out all the neighborhoods that do not have any names
+            IEnumerable<Properties> result = FilterOutNoNeighbors(list);
+            Print(result);
+
+            ///3.Remove the Duplicates
+            RemoveDuplicates(list);
+
+            ///4.Rewrite the queries from above, and consolidate all into one single query.
+            
+
+        }
+
     }
 }
