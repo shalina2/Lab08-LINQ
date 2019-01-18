2eaf25811309a08c8cd2775990913bc5495a97fd
shalina2 <shalom.belaineh@gmail.com>
2019-01-17 19:16:24 -0800

    added path

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -7,7 +7,7 @@ namespace LINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string Path=(./.././)
-        }
+            string Path = ("./../../../../../../../Linq.JSON ");
+         }
     }
 }
