c6afe1c60833635aaac3a132c1ba855875a6c6b0
shalina2 <shalom.belaineh@gmail.com>
2019-01-17 18:54:37 -0800

    added classes

---
+++ LINQ/Classes/RootObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ.Classes
+{
+    public class RootObject
+    {
+        public string type { get; set; }
+        public List<Feature> features { get; set; }
+    }
+}
